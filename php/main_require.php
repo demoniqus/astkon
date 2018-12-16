829d4f2fb1cfea41d71731ee9c9cc3bd934a31bf
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-16 15:46:24 +0300

    Файл main_require по состоянию на 16.12.2018 15:45

---
+++ php/main_require.php
@@ -0,0 +1,86 @@
+<?php
+namespace Astkon;
+
+
+/*
+ * Главный файл с подключаемыми модулями
+ */
+/*
+ * Устанавливаем кодировку данных
+ */
+header('Content-Type: text/html; charset=utf-8');
+require_once 'core/Model.php';
+require_once 'core/Controller.php';
+require_once 'base64.php';
+require_once 'db.php';
+require_once 'linq.php';
+require_once 'types_processor.php';
+require_once 'file_system.php';
+require_once 'url.php';
+require_once 'models.php';
+require_once 'controllers.php';
+
+
+/*Глобальные константы*/
+final class GlobalConst {
+    /**
+     * Наименование хоста, на котором располагается БД
+     * @var string
+     */
+    const Host = 'localhost';
+    /**
+     * Наименование БД
+     * @var string
+     */
+    const DbName = 'astkon';
+    /**
+     * Логин и пароль для подключения к БД
+     * @var string
+     */
+    const HostUser = 'root';
+    const HostPass = '1';
+    /**
+     * Наименование параметра, определяющего тип запроса - получение данных или получение html-страницы
+     * @var string
+     */
+    const DataModeKey = 'mode';
+    /**
+     * Наименование параметра, определяющего запрашиваемые данные
+     * @var string
+     */
+    const DataKeyKey = 'datakey';
+    /**
+     * Наименование параметра, определяющего запрашиваемую html-страницу
+     * @var string
+     */
+    const RequiredPageKey = 'page';
+    /**
+     * Уровень прав для вновь создаваемых каталогов:
+     * 0777 полный доступ на чтение и изменение каталога всеми пользователями
+     * и группами пользователей
+     * @var int
+     */
+    const DefDirAccess = 0777;
+    /**
+     * Временный каталог проекта
+     * @var string
+     */
+    const TmpDirName = 'tmp';
+    /**
+     * Каталог для создания частичных классов, реализующих сущности базы данных
+     * @var string
+     */
+    const PartialClassDirectory = 'php' . DIRECTORY_SEPARATOR . 'partialModels';
+    /**
+     * Каталог для классов, реализующих частичные классы
+     * @var string
+     */
+    const ClassDirectory = 'php' . DIRECTORY_SEPARATOR . 'models';
+    /**
+     * Файл для регистрации моделей, реализующих сущности базы данных
+     */
+    const ClassRegistry = 'php' . DIRECTORY_SEPARATOR . 'models.php';
+    const FOpenMode = 'wt';
+
+}
+
