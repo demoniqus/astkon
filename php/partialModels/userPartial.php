35d90765f9db952ff23a25a2536ca6dbbc7a4283
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-16 15:52:53 +0300

    Добавлены некоторые модели

---
+++ php/partialModels/userPartial.php
@@ -0,0 +1,19 @@
+<?php 
+
+/** Generated automaticaly. Don't change this file manually! */
+
+
+namespace Astkon\Model\Partial;
+use Astkon\Model\Model;
+abstract class UserPartial extends Model {
+	const DataTable = 'user';
+	/** @var int */
+	public $IdUser;
+
+	/** @var string */
+	public $Login;
+
+	/** @var string */
+	public $Password;
+
+}
