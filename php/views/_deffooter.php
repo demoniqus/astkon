b33fae7c0a72b612edf580f978466b038f9f67bb
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-17 15:23:55 +0300

    Сделаны вьюхи header, footer, left_menu

--- php/views/_deffooter.php
+++ php/views/_deffooter.php
@@ -1,3 +1,27 @@
 
+<!-- https://toster.ru/q/19238 -->
+<style type="text/css">
+
+</style>
+</div>
+<footer id="document-footer">
+    <div class="container-fluid">
+        demonius
+    </div>
+</footer>
+<script type="text/javascript">
+    (function(){
+        var f = function(){
+            var docBody = document.getElementById('document-body');
+            var docFooter = document.getElementById('document-footer');
+            var docHeader = document.getElementById('document-header');
+
+            $(docBody).height(document.body.clientHeight - docFooter.offsetHeight - docHeader.offsetHeight);
+            console.log(window.innerHeight + ' => ' + docFooter.offsetHeight + ' => ' + docBody.offsetHeight)
+        };
+        window.addEventListener('resize', f)
+        f();
+    })()
+</script>
 </body>
 </html>
