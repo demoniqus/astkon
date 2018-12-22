b8a11a87156e85c847d85c7e534a24a5e7956345
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-22 23:41:35 +0300

    Checkbox требует value, приводимое к true, иначе возввращает значение on

--- php/views/_form_edit_fields/boolean.php
+++ php/views/_form_edit_fields/boolean.php
@@ -2,7 +2,7 @@
     <label class="col-sm-3 col-form-label text-right"><?= $alias; ?></label>
     <div class="col-sm-9 col-lg-5">
         <input type="hidden" name="<?= $propName; ?>" value="" />
-        <input type="checkbox" name="<?= $propName; ?>" class="" <?= ($value ? 'checked="CHECKED"' : ''); ?>" />
+        <input type="checkbox" name="<?= $propName; ?>" class="" <?= ($value ? 'checked="CHECKED"' : ''); ?>" value="1" />
     </div>
 </div>
 <?php
