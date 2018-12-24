4508c5e3ed6dd9ad8805d44a56d315ee871c0d10
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-24 18:31:17 +0300

    Подключен на страницу JQuery UI

--- php/views/_defheader.php
+++ php/views/_defheader.php
@@ -3,7 +3,8 @@
 use Astkon\GlobalConst;
 
 $bootstrapDir = '/bootstrap_v4.0.0';
-
+$jqueryVers = '3.3.1';
+$jqueryUIVers = '1.12.1';
 ?>
 <!doctype html>
 <html lang="ru" xmlns="http://www.w3.org/1999/xhtml">
@@ -12,13 +13,17 @@ $bootstrapDir = '/bootstrap_v4.0.0';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Astkon Warehouse</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/jquery-ui-<?= $jqueryUIVers;?>/jquery-ui.css" />
     <link rel="stylesheet" href="<?= $bootstrapDir; ?>/bootstrap.css" />
     <link rel="stylesheet" href="<?= $bootstrapDir; ?>/bootstrap-grid.css" />
     <link rel="stylesheet" href="<?= $bootstrapDir; ?>/bootstrap-reboot.css" />
     <link rel="stylesheet" href="/main.css" />
-    <script type="text/javascript" src="/jquery/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/jquery/jquery-<?= $jqueryVers;?>.min.js"></script>
+    <script type="text/javascript" src="/jquery/jquery-ui-<?= $jqueryUIVers;?>/jquery-ui.min.js"></script>
     <script type="text/javascript" src="<?= $bootstrapDir; ?>/bootstrap.js" ></script>
     <script type="text/javascript" src="<?= $bootstrapDir; ?>/bootstrap.bundle.js" ></script>
+    <script type="text/javascript" src="/linq.js" ></script>
+    <script type="text/javascript" src="/main.js" ></script>
     <style type="text/css">
         body, html {
             width: 100% !important;
