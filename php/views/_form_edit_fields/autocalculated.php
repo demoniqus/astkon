f974fd6db8eecd661ac8cadc6ef527f3179a36d3
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-24 18:59:05 +0300

    Вывод результатов проверки значений полей. Для foreign_key также вывод читабельного значения вместо внешнего ключа

--- php/views/_form_edit_fields/autocalculated.php
+++ php/views/_form_edit_fields/autocalculated.php
@@ -2,7 +2,7 @@
     <label class="col-sm-3 col-form-label text-right"><?= $alias; ?></label>
     <div class="col-sm-9 col-lg-5">
         <input type="hidden" name="<?= $propName; ?>" value="" />
-        <input type="checkbox" name="<?= $propName; ?>" class="" <?= ($value ? 'checked="CHECKED"' : ''); ?>" />
+        <div class="col-sm-10 visible-value"><?= $value; ?></div>
     </div>
 </div>
 <?php
--- php/views/_form_edit_fields/foreign_key.php
+++ php/views/_form_edit_fields/foreign_key.php
@@ -1,10 +1,18 @@
+<?php
+
+require getcwd() . DIRECTORY_SEPARATOR . \Astkon\GlobalConst::ViewsDirectory . DIRECTORY_SEPARATOR . '_form_edit_fields' . DIRECTORY_SEPARATOR . '_valid_state.php';
+
+use Astkon\DataBase;
+
+?>
 <div class="form-group row <?= $propName; ?>">
     <label class="col-sm-3 col-form-label text-right"><?= $alias; ?></label>
     <div class="col-sm-9 col-lg-5 row">
         <div class="col-sm-2">
-            <button type="button" class="btn btn-light">...</button>
+            <button type="button" class="btn btn-light" onclick="DictionarySelector.dialog($(this).parents('.form-group:first'), '<?= DataBase::underscoreToCamelCase($ForeignKeyParams['field']); ?>', '<?= $dictionaryAction; ?>')">...</button>
         </div>
-        <input type="hidden" name="<?= $propName; ?>" value="<?= $value; ?>" />
         <div class="col-sm-9 visible-value"><?= $displayValue; ?></div>
+        <input type="hidden" name="<?= $propName; ?>" value="<?= $value; ?>" class="form-control <?= $validClass; ?>"/>
+        <?= $feedback; ?>
     </div>
 </div>
