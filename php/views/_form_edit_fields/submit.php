77de2cb5f03b2516137abc3d00c3b4da7a7eacb1
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-27 16:12:47 +0300

    В связи с трудностью определения URL для кнопки "Отмена" создана кнопка "Вернуться в список"

--- php/views/_form_edit_fields/submit.php
+++ php/views/_form_edit_fields/submit.php
@@ -1,4 +1,10 @@
 <div class="form-group row">
     <button type="submit" name="submit" class="btn btn-primary">Сохранить</button>
-    <button type="button" class="btn btn-outline-danger ml-3" onclick="document.location.href='<?= isset($options['Rollback']) ? $options['Rollback'] : $_SERVER['HTTP_REFERER']; ?>'">Oтмена</button>
+    <?php if (isset($options['backToList'])) {
+        ?>
+        <button type="button" class="btn btn-outline-secondary ml-3" onclick="document.location.href='<?= $options['backToList']; ?>'">Вернуться в список</button>
+        <?php
+    }
+    ?>
+    <button type="button" class="btn btn-outline-danger ml-3" style="display: none !important;" onclick="document.location.href='<?= isset($options['Rollback']) ? $options['Rollback'] : $_SERVER['HTTP_REFERER']; ?>'">Oтмена</button>
 </div>
