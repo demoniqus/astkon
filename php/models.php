7d44c32def71b7942da4e894eaf7ab92d2a7e9e7
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-27 14:51:34 +0300

    Зарегистрированы новые модели, связанные с типом Operation

--- php/models.php
+++ php/models.php
@@ -4,3 +4,7 @@ require_once getcwd() . '/php/models/Measure.php';
 require_once getcwd() . '/php/models/User.php';
 require_once getcwd() . '/php/models/BuildObject.php';
 require_once getcwd() . '/php/models/People.php';
+require_once getcwd() . '/php/models/Operation.php';
+require_once getcwd() . '/php/models/OperationType.php';
+require_once getcwd() . '/php/models/OperationItem.php';
+require_once getcwd() . '/php/models/OperationState.php';
