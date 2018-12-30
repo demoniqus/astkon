67a686111786c553154058a26e7561a4720f2070
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-30 18:56:00 +0300

    Правильная ссылка на форму редактирования для новой группы пользователей

--- php/views/usergroups/usergroupslist.php
+++ php/views/usergroups/usergroupslist.php
@@ -10,7 +10,7 @@
     require_once getcwd() . DIRECTORY_SEPARATOR . GlobalConst::ViewsDirectory . DIRECTORY_SEPARATOR . 'left_menu.php'; ?>
     <div class="col-lg text-center">
         <div class="text-left">
-            <a href="/Articles/Edit/0" class="btn btn-success">Добавить артикул</a>
+            <a href="/UserGroups/Edit/0" class="btn btn-success">Добавить группу</a>
         </div>
         <?php
         View::TableList($modelConfig, $listItems, $listItemOptions);
