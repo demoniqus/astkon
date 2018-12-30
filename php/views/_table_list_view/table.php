e650eb791bda49a98e925e4ab63e44ae9e4aadda
Antipov Dmitriy <demoniqus@mail.ru>
2018-12-30 18:13:18 +0300

    Добавлена стилизация для иконки логического поля

--- php/views/_table_list_view/table.php
+++ php/views/_table_list_view/table.php
@@ -1,6 +1,17 @@
 <?php
 
 ?>
+<style type="text/css">
+    .bool-field-true-value-icon {
+        width: 24px;
+        height: 24px;
+        max-width: 24px;
+        max-height: 24px;
+        min-width: 24px;
+        min-height: 24px;
+        border: 0px none;
+    }
+</style>
 <script type="text/javascript">
 
 </script>
