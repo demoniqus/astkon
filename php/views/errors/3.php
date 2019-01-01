5bb5eabf51bcf7910ef214c4d2b92b9cb9f7821c
Antipov Dmitriy <demoniqus@mail.ru>
2019-01-01 17:06:05 +0300

    Вьюха для ошибки с кодом HACKING

--- php/views/errors/3.php
+++ php/views/errors/3.php
@@ -1,11 +1,9 @@
 <?php
 
-use Astkon\GlobalConst;
-
 ?>
 <style type="text/css">
     body {
-        background-image: url(/backgrounds/disaster.jpg);
+        background-image: url(/backgrounds/hacker_must_die.jpg);
         background-attachment: fixed;
         background-size: 100%;
     }
@@ -19,11 +17,4 @@ use Astkon\GlobalConst;
     <div class="col pl-3 pt-3 m-0">
         <a href="/" style="font-size: 150%; color: #005cbf;">На главную страницу</a>
     </div>
-    <?php
-    if (isset($trace)) {
-        echo '<div class="row m-0 text-left px-3" style="font-weight: bold; color: lightcoral">';
-        echo $trace;
-        echo '</div>';
-    }
-    ?>
 </div>
