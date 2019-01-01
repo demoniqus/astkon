efcefdacfc8110b0379b6af45e8c1a087ca9d2f4
Antipov Dmitriy <demoniqus@mail.ru>
2019-01-01 10:08:51 +0300

    Вьюха для ошибки 403 - FORBIDDEN

---
+++ php/views/errors/403.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+<style type="text/css">
+    body {
+        background-image: url(/backgrounds/access-denied.png);
+        background-attachment: fixed;
+        background-size: 100%;
+    }
+</style>
+<div class="container-fluid">
+    <div class="col pl-3 pt-3 m-0" style="font-size: 300%; color: #005cbf;">
+        403
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="col pl-3 pt-3 m-0">
+        <a href="/" style="font-size: 150%; color: #005cbf;">На главную страницу</a>
+    </div>
+</div>
