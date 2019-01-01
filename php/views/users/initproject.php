1d7437875197cedc24a52ab508669fa8175cfc6d
Antipov Dmitriy <demoniqus@mail.ru>
2019-01-01 16:45:59 +0300

    Новые фоновые изображения

---
+++ php/views/users/initproject.php
@@ -0,0 +1,39 @@
+<?php
+
+use Astkon\GlobalConst;
+use Astkon\Model\Model;
+
+?>
+<form method="post" action="Auth/Index">
+    <div class="row mx-0">
+        <div class="col-lg text-center">
+            <div class="mx-5 alert alert-secondary text-center">
+                Авторизуйтесь
+            </div>
+            <?php
+            $validMessage = '';
+            $value = '';
+            if ($validState === Model::ValidStateError) {
+                $validationFormClass = 'alert-danger';
+                $validationFormMessage = 'Пользователя с такими данными не существует';
+                require GlobalConst::ViewsDirectory . DIRECTORY_SEPARATOR . '_form_edit_fields' . DIRECTORY_SEPARATOR . 'common_message.php';
+            }
+
+            $inputType = 'text';
+            $alias = 'Логин';
+            $propName = 'Login';
+            require GlobalConst::ViewsDirectory . DIRECTORY_SEPARATOR . '_form_edit_fields' . DIRECTORY_SEPARATOR . 'input.php';
+
+            $inputType = 'password';
+            $alias = 'Пароль';
+            $propName = 'Password';
+            require GlobalConst::ViewsDirectory . DIRECTORY_SEPARATOR . '_form_edit_fields' . DIRECTORY_SEPARATOR . 'input.php';
+            ?>
+            <div class="row">
+                <div class="col text-center">
+                    <button type="submit" name="submit" class="btn btn-primary">Войти</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
