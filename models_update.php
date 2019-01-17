b527f57e92085e7fc2bbfeeeaa025f25b5c6a0ac
Антипов Дмитрий <demoniqus@mail.ru>
2019-01-17 15:40:12 +0300

    Сделан отдельный файл для запуска процедуры обновления моделей

---
+++ models_update.php
@@ -0,0 +1,22 @@
+<?PHP
+namespace Astkon;
+
+use Astkon\Model\Model;
+use Astkon\View\View;
+
+require_once  '.' . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'main_require.php';
+
+try {
+    Model::UpdateModelPhpCode();
+}
+catch (\Exception $exception) {
+    $view = new View();
+    $view->trace = array(
+        'errorCode' => $exception->getCode(),
+        'errorMessage' => $exception->getMessage(),
+        'trace' => $exception->getTrace()
+    );
+
+    $view->error(ErrorCode::PROGRAMMER_ERROR);
+    die();
+}
