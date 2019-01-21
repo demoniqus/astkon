d3dd5f15c63e8141ac34c20ca2820a392af345ba
Антипов Дмитрий <demoniqus@mail.ru>
2019-01-21 15:56:15 +0300

    Для таблицы определяется наименование первичного ключа

--- php/views/_table_list_view/table.php
+++ php/views/_table_list_view/table.php
@@ -22,6 +22,13 @@
     </script>
     <table class="table table-hover my-2 table-sm" id="<?= $tableViewConfig->id; ?>">
     <?php
+    $primaryKeyName = array_filter(
+        $config,
+        function($configItem){
+            return $configItem['primary_key'];
+        }
+    )[0]['key'];
+
     require_once TABLE_LIST_VIEW_DIRECTORY . DIRECTORY_SEPARATOR . 'thead.php';
     require_once TABLE_LIST_VIEW_DIRECTORY . DIRECTORY_SEPARATOR . 'tbody.php';
     ?>
